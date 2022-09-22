572bcf4a75241760b34662a310660e5e44e43146
Kerolos Lotfy <100154566+KerolosLotfy@users.noreply.github.com>
2022-09-23 00:28:02 +0200

    add bg image

--- js/app.js
+++ js/app.js
@@ -54,37 +54,58 @@ enterBtn.addEventListener('click', () => {
 })
 
 // Add New Task to page
-function addNewTask(value, state = false) {
+function addNewTask(value, state = false, imgSrc = 'url("")') {
     let task = {
+        id: Date.now(),
         task_title: value,
-        complate: state
+        complate: state,
+        imgSrc: imgSrc,
     }
 
     if (task.task_title !== '') {
         // create Elements 
         let li = createElementAndAppendToParent('li', '', '', document.querySelector('.tasks'));
-        li.dataset.complate = task.complate;
+        li.setAttribute("id", `${task.id}`);
+        li.style.backgroundImage = task.imgSrc;
         let p = createElementAndAppendToParent('p', value, '', li);
         let control = createElementAndAppendToParent('div', '', 'control', li);
         let editIcon = createElementAndAppendToParent('i', '', 'fa-solid fa-pen-to-square edit', control);
         let DeleteIcon = createElementAndAppendToParent('i', '', 'fa-solid fa-trash delete', control);
         let checkIcon = createElementAndAppendToParent('i', '', 'fa-solid fa-circle-check check', control);
         checkIcon.setAttribute("title", 'uncomplated')
+        let imageIcon = createElementAndAppendToParent('label', '', 'fa-solid fa-image', control);
+        imageIcon.setAttribute("for", 'bgImage');
+        let inputFile = createElementAndAppendToParent('input', '', '', imageIcon);
+        inputFile.setAttribute("type", 'file');
+        inputFile.setAttribute("id", 'bgImage');
+        inputFile.setAttribute("title", 'bgimage');
+        let calendar = createElementAndAppendToParent('label', '', 'fa-regular fa-calendar', control);
+        calendar.setAttribute("for", 'remember');
+        let remember = createElementAndAppendToParent('input', '', 'fa-solid fa-calendar-days', calendar);
+        remember.setAttribute("type", 'datetime-local');
+        remember.setAttribute("id", 'remember');
+        remember.setAttribute("title", 'remember');
 
-        // add delete Task function for when click on delete Icon
-        DeleteIcon.onclick = deleteTask(DeleteIcon);
+        // add delete Task function  when click on delete Icon
+        deleteTask(DeleteIcon);
 
-        // add edit Task function for when click on edit Icon
-        editIcon.onclick = editTask(editIcon);
+        // add edit Task function  when click on edit Icon
+        editTask(editIcon);
 
-        // add check Task function for when click on check Icon
-        checkIcon.onclick = checkTask(checkIcon);
+        // add check Task function  when click on check Icon
+        checkTask(checkIcon, task.imgSrc);
 
-        // add new task (object) to array
-        arrOfTasks.push(task);
+        //  add Background Image function  when click on image Icon
+        addBgImage(inputFile, task.imgSrc);
+
+        //  add Background Image function  when click on image Icon
+        rememberFun(remember);
 
         // update tasks list
         tasks_list = document.querySelectorAll(".tasks li");
+
+        // add new task (object) to array
+        arrOfTasks.push(task);
 
         // add Data In Local Storage  function(array, itemName as 'string')
         addDataInLocalStorage(arrOfTasks, 'tasks');
@@ -99,25 +120,25 @@ function addDataInLocalStorage(arrOfTasks, itemName = 'item') {
 // Get Data Form Local Storage when loading the page 
 function getDataformLocalStorage(arrOfTasks) {
     let arr = JSON.parse(arrOfTasks);
-    arr.forEach((task, i, arr) => {
-        if (task.complate) {
+    arr.forEach((obj, i, arr) => {
+        if (obj.complate) {
             arrOfComplateTasks = arr;
-            addComplatedTask(task.task_title, task.id);
-        } else if (task.bgColor) {
-            control_colors[0].children[1].value = task.bgColor;
-            control_colors[1].children[1].value = task.textColor;
-            control_colors[2].children[1].value = task.activeColor;
-            applyColorsInPage(task);
+            addComplatedTask(obj.task_title, obj.id, obj.imgSrc);
+        } else if (obj.bgColor) {
+            control_colors[0].children[1].value = obj.bgColor;
+            control_colors[1].children[1].value = obj.textColor;
+            control_colors[2].children[1].value = obj.activeColor;
+            applyColorsInPage(obj);
         } else {
-            addNewTask(task.task_title);
+            addNewTask(obj.task_title, '', obj.imgSrc);
         }
     })
 }
 
 // Create Element And Append This Element To Parent Element 
-function createElementAndAppendToParent(element = '', innerText = '', className = '', parent) {
+function createElementAndAppendToParent(element = '', innerHTML = '', className = '', parent) {
     let el = document.createElement(element);
-    el.innerText = innerText;
+    el.innerHTML = innerHTML;
     el.className = className;
     parent.appendChild(el)
     return el;
@@ -126,24 +147,30 @@ function createElementAndAppendToParent(element = '', innerText = '', className 
 // Delete Task function
 function deleteTask(del) {
     del.addEventListener("click", () => {
-        del.parentElement.parentElement.remove();
-        // Update Data In Local Storage
-        updateData();
+        let alert = confirm('Are you sure you want delete this task?')
+        if (alert) {
+            del.parentElement.parentElement.remove();
+            // Update Data In Local Storage
+            updateData();
+        }
+
     });
 };
 
 // Edit Task function
 function editTask(edit) {
     edit.addEventListener('click', () => {
-        if (!edit.parentElement.parentElement.children[0].hasAttribute("contenteditable")) {
-            edit.classList.add("active");
-            edit.parentElement.parentElement.children[0].setAttribute("contenteditable", "true")
-            edit.parentElement.parentElement.children[0].focus();
-        } else {
-            edit.parentElement.parentElement.children[0].removeAttribute("contenteditable");
+        if (edit.parentElement.parentElement.children[edit.parentElement.parentElement.children.length - 2].hasAttribute("contenteditable")) {
+            edit.parentElement.parentElement.children[edit.parentElement.parentElement.children.length - 2].removeAttribute("contenteditable");
+            edit.parentElement.parentElement.children[edit.parentElement.parentElement.children.length - 2].blur();
+            console.log('adadadad');
             edit.classList.remove("active");
             // Update Data In Local Storage
             updateData();
+        } else {
+            edit.classList.add("active");
+            edit.parentElement.parentElement.children[edit.parentElement.parentElement.children.length - 2].setAttribute("contenteditable", "true");
+            edit.parentElement.parentElement.children[edit.parentElement.parentElement.children.length - 2].focus();
         };
 
     })
@@ -151,23 +178,44 @@ function editTask(edit) {
 }
 
 // when click Check icon make a Task complated  
-function checkTask(c) {
+function checkTask(c, imgSrc = '') {
     c.addEventListener("click", () => {
-        // add date complated task
-        let date = new Date().toLocaleString('en-Au', { dateStyle: 'short', timeStyle: 'short', 'hourCycle': 'h12' });
-        // add Complated Task to  Complated tasks list
-        addComplatedTask(c.parentElement.parentElement.children[0].innerText, date);
-        // Update Data In Local Storage
-        updateData(c.parentElement.parentElement.children[0].innerText, date);
-        // remove complated task from tasks list
-        c.parentElement.parentElement.remove();
+        let alert = confirm('Are you sure you have completed this task?')
+        if (alert) {
+            // add date complated task
+            let date = new Date().toLocaleString('en-Au', { dateStyle: 'short', timeStyle: 'short', 'hourCycle': 'h12' });
+            // add Complated Task to  Complated tasks list
+            addComplatedTask(c.parentElement.parentElement.children[0].innerText, date, imgSrc);
+            // Update Data In Local Storage
+            updateData(c.parentElement.parentElement.children[0].innerText, date);
+            // remove complated task from tasks list
+            c.parentElement.parentElement.remove();
+        }
     });
 }
 
+// when click Check icon add Background Image 
+function addBgImage(inputFile) {
+    inputFile.addEventListener('change', () => {
+        let reader = new FileReader();
+        reader.readAsDataURL(inputFile.files[0]);
+        reader.addEventListener('load', (e) => {
+            inputFile.parentElement.parentElement.parentElement.style.backgroundImage = `url('${reader.result}')`;
+            updateData();
+        });
+    })
+}
+
+// when click calunder icon make remember fir task  
+function rememberFun(remb) {
+    return false;
+}
+
 // add Complated Task to page 
-function addComplatedTask(task_title, date = '') {
+function addComplatedTask(task_title, date = '', imgSrc = 'url("")') {
     // create Element And Append To Parent
     let li = createElementAndAppendToParent('li', '', '', document.querySelector('.complated > ul'));
+    li.style.backgroundImage = imgSrc;
     let p = createElementAndAppendToParent('p', task_title, '', li);
     let control = createElementAndAppendToParent('div', '', 'control', li);
     let checkIcon = createElementAndAppendToParent('i', '', 'fa-solid fa-circle-check check', control);
@@ -183,13 +231,16 @@ function updateData(complatedTask = '', date = '') {
     arrOfTasks = [];
     tasks_list.forEach((li, i) => {
         let task = {
+            id: date,
             task_title: li.innerText,
             complate: false,
+            imgSrc: li.style.backgroundImage
         }
         if (task.task_title === complatedTask) {
             task.complate = true;
             task.id = date;
             arrOfComplateTasks.push(task);
+            console.log(arrOfComplateTasks);
         } else {
             arrOfTasks.push(task);
         }
@@ -198,6 +249,7 @@ function updateData(complatedTask = '', date = '') {
     addDataInLocalStorage(arrOfComplateTasks, 'complated_tasks');
 }
 
+// ***************************************************************************************************
 // click broom icon to Detele All complated_tasks 
 clearComplatedTasks.addEventListener('click', () => {
     complatedTasks = document.querySelectorAll('.complated > ul li');
@@ -212,7 +264,7 @@ menuBtn.addEventListener(("click"), () => {
     menuBtn.classList.toggle("active");
     document.querySelector('menu').classList.toggle("active");
     control_colors.forEach((form) => {
-        form.children[1].addEventListener('blur', () => {
+        form.children[1].addEventListener('input', () => {
             document.querySelector('menu').classList.remove("active");
             let colors = {
                 bgColor: control_colors[0].children[1].value,
